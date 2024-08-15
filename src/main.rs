f4558333db91af7d17423fe09677f5ba42df44ea
jam.jar <darkestknight3d@outlook.com>
2024-08-14 19:00:00 -0700

    Implemented Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
+mod vec3;
+use std::io::{self, Write};
+
+use vec3::*;
 fn main() {
+    let image_width = 1024;
+    let image_height = 1024;
+    println!("P3\n{} {}\n255\n", image_width, image_height);
 
-    let image_width = 16;
-    let image_height = 16;
-
-    print!("P3\n{} {}\n255\n", image_width, image_height);
-
-    for j in 0..=image_height-1 {
-        for i in 0..=image_width-1 {
+    for j in 0..=image_height - 1 {
+        for i in 0..=image_width - 1 {
             let r = i as f64 / (image_height) as f64;
             let g = j as f64 / (image_width) as f64;
             let b = 0.0;
 
-            println!("{} {} {}", (r*255.999 )as u32, (g*255.999) as u32, (b*255.999) as u32)
-
+            println!(
+                "{} {} {}",
+                (r * 255.999) as u32,
+                (g * 255.999) as u32,
+                (b * 255.999) as u32
+            );
         }
+        eprint!("\r {}/{} lines rendered", j + 1, image_height)
     }
-
 }
---
+++ src/vec3.rs
@@ -0,0 +1,169 @@
+#[allow(dead_code)]
+use std::ops::{
+    Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub, SubAssign,
+};
+#[derive(Debug)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+type Point = Vec3;
+impl Vec3 {
+    //new(x,y,z)
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    //length() - returns the length of a vector
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt() //TODO: implement fast inverse square root
+    }
+    //length_squared() - the length of a vector squared ( source of length )
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn normalized(self) -> Self {
+        let length: f64 = self.length();
+        return self / length;
+    }
+}
+
+pub fn dot(a: &Vec3, b: &Vec3) -> f64 {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+pub fn cross(a: &Vec3, b: &Vec3) -> Vec3 {
+    Vec3 {
+        x: a.y * b.z - a.z * b.y,
+        y: a.z * b.x - a.x * b.z,
+        z: a.x * b.y - a.y * b.x,
+    }
+}
+
+impl From<f64> for Vec3 {
+    fn from(n: f64) -> Self {
+        Vec3 { x: n, y: n, z: n }
+    }
+}
+//allows indexing the Vector with (unsigned) integer values, both mutable and immutable  eg. vec3[1] = vec3.y
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("Index out of bounds"),
+        }
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            _ => panic!("Index out of bounds"),
+        }
+    }
+}
+
+//vec3 Neg implemntation, allows use of -vec3
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Clone for Vec3 {
+    fn clone(&self) -> Vec3 {
+        Vec3 {
+            x: self.x,
+            y: self.y,
+            z: self.z,
+        }
+    }
+}
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: f64) -> Vec3 {
+        Vec3 {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, other: f64) -> Vec3 {
+        Vec3 {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+impl Div<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+//point as an alias
+
+//Basic functions
