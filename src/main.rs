daa8bec0eba1afe2d344e994fbd4c0e9d704a2a6
jam.jar <darkestknight3d@outlook.com>
2024-08-14 19:57:00 -0700

    Created Ray struct and methods, removed unused dependency.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
+mod color;
+mod ray;
 mod vec3;
 use std::io::{self, Write};
 
+use color::*;
+use ray::*;
 use vec3::*;
 fn main() {
-    let image_width = 1024;
-    let image_height = 1024;
+    let image_width = 256;
+    let image_height = 256;
     println!("P3\n{} {}\n255\n", image_width, image_height);
 
     for j in 0..=image_height - 1 {
         for i in 0..=image_width - 1 {
-            let r = i as f64 / (image_height) as f64;
-            let g = j as f64 / (image_width) as f64;
-            let b = 0.0;
+            let pixel_color = Color {
+                x: i as f64 / (image_height) as f64,
+                y: j as f64 / (image_width) as f64,
+                z: 0.0,
+            };
 
-            println!(
-                "{} {} {}",
-                (r * 255.999) as u32,
-                (g * 255.999) as u32,
-                (b * 255.999) as u32
-            );
+            write_color(pixel_color);
         }
         eprint!("\r {}/{} lines rendered", j + 1, image_height)
     }
